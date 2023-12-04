8d38ad50d59a0f32b84feb1fa9e4ed3abd43e5cb
Biedr <pietrococozza1@gmail.com>
2023-12-04 22:12:44 +0100

    first commit

---
+++ conteggio-ore/src/main/java/com/conteggio/ore/conteggioore/ConteggioOreApplication.java
@@ -0,0 +1,13 @@
+package com.conteggio.ore.conteggioore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConteggioOreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConteggioOreApplication.class, args);
+	}
+
+}
---
+++ conteggio-ore/src/test/java/com/conteggio/ore/conteggioore/ConteggioOreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conteggio.ore.conteggioore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConteggioOreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
